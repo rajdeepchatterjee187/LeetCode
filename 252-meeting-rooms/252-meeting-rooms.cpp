03ec8c1ae97d6dc501cd181ead17085efcf23b11
rajdeepchatterjee187 <62827471+rajdeepchatterjee187@users.noreply.github.com>
2022-07-04 21:19:37 +0530

    Time: 35 ms (26.45%), Space: 12.3 MB (16.54%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cpp
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> vec;
+        vector<int> vec;
         for(auto interval : intervals) {
-            if(vec.empty() || vec.back()[1] <= interval[0]) {
-                vec.push_back(interval);
+            if(vec.empty() || vec.back() <= interval[0]) {
+                vec.push_back(interval[1]);
             }
             else return false;
         }
