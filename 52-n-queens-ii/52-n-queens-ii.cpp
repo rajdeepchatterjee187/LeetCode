b03ca507bd85b358f5a821672c0dbd1c5829a05d
rajdeepchatterjee187 <62827471+rajdeepchatterjee187@users.noreply.github.com>
2022-07-18 11:18:42 +0530

    Time: 32 ms (15.71%), Space: 7.5 MB (19.12%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<string>> result;
+    int ans;
     int size;
     
     int totalNQueens(int n) {
         size = n;
         
-        vector<string> board(n, string(n, '.'));
+        
         unordered_set<int> cols;
         unordered_set<int> diags;
         unordered_set<int> antidiags;
         
-        backtrack(0, cols, diags, antidiags, board);
-        return result.size();
+        backtrack(0, cols, diags, antidiags);
+        return ans;
     }
     
     void backtrack(int row, unordered_set<int>& cols, unordered_set<int>& diags,
-                  unordered_set<int>& antidiags, vector<string>& board) {
+                  unordered_set<int>& antidiags) {
         //base case
-        if(row == size)    result.push_back(board);
+        if(row == size)    ans++;
         
         //check for every column of a particular row
         for(int col = 0; col < size; col++) {
@@ -34,16 +34,16 @@ public:
             cols.insert(col);
             diags.insert(Diag);
             antidiags.insert(AntiDiag);
-            board[row][col] = 'Q';
+            
             
             //move to the next row
-            backtrack(row+1, cols, diags, antidiags, board);
+            backtrack(row+1, cols, diags, antidiags);
             
             //remove the queen i.e. backtrack
             cols.erase(col);
             diags.erase(Diag);
             antidiags.erase(AntiDiag);
-            board[row][col] = '.';
+            
         }
     }
 };
