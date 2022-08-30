ddd31438403cdc06e2f42b217783dcadc633ccdc
rajdeepchatterjee187 <62827471+rajdeepchatterjee187@users.noreply.github.com>
2022-08-30 14:09:57 +0530

    Time: 3 ms (88.91%), Space: 11 MB (75.19%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size()-1;
         
-        auto it = find(nums.begin(), nums.end(), target);
-        if(it == nums.end())    return -1;
-        else return it - nums.begin();
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[start] <= nums[mid]){                  //the left part is sorted
+                if(target >= nums[start] and target <= nums[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+            else {                      //the right part is sorted
+                if(target >= nums[mid] and target <= nums[end])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+        }
+        
+        return -1;
     }
 };
