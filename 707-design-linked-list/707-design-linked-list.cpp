5bd0ab89ce9bf5d30904ca0c1268816331e1e158
rajdeepchatterjee187 <62827471+rajdeepchatterjee187@users.noreply.github.com>
2022-09-12 13:58:00 +0530

    Time: 94 ms (11.44%), Space: 19.6 MB (67.04%) - LeetHub

---
+++ 707-design-linked-list/707-design-linked-list.cpp
@@ -0,0 +1,91 @@
+struct SinglyListNode {
+    int val;
+    SinglyListNode *next;
+    SinglyListNode(int x) : val(x), next(NULL) {}
+};
+
+class MyLinkedList {
+private:
+    SinglyListNode *head;
+public:
+    MyLinkedList() {
+        head = NULL;
+    }
+    
+    SinglyListNode* getNode(int index) {
+    SinglyListNode *cur = head;
+    for (int i = 0; i < index && cur; ++i) {
+        cur = cur->next;
+    }
+    return cur;
+}
+    SinglyListNode* getTail() {
+    SinglyListNode *cur = head;
+    while (cur && cur->next) {
+        cur = cur->next;
+    }
+    return cur;
+}
+    
+    int get(int index) {
+         SinglyListNode *cur = getNode(index);
+    return cur == NULL ? -1 : cur->val;
+    }
+    
+    void addAtHead(int val) {
+            SinglyListNode *cur = new SinglyListNode(val);
+    cur->next = head;
+    head = cur;
+    return;
+    }
+    
+    void addAtTail(int val) {
+        if (head == NULL) {
+        addAtHead(val);
+        return;
+    }
+    SinglyListNode *prev = getTail();
+    SinglyListNode *cur = new SinglyListNode(val);
+    prev->next = cur;
+    }
+    
+    void addAtIndex(int index, int val) {
+         if (index == 0) {
+        addAtHead(val);
+        return;
+    }
+    SinglyListNode *prev = getNode(index - 1);
+    if (prev == NULL) {
+        return;
+    }
+    SinglyListNode *cur = new SinglyListNode(val);
+    SinglyListNode *next = prev->next;
+    cur->next = next;
+    prev->next = cur;
+    }
+    
+    void deleteAtIndex(int index) {
+        SinglyListNode *cur = getNode(index);
+    if (cur == NULL) {
+        return;
+    }
+    SinglyListNode *next = cur->next;
+    if (index == 0) {
+        // modify head when deleting the first node.
+        head = next;
+    } else {
+        SinglyListNode *prev = getNode(index - 1);
+        prev->next = next;
+    }
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
